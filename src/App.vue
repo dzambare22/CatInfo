6499d98e37c05487d99bc686a1c0aa3a7860bf8c
ZAMBARE <dhanashri-rahul.zambare@capgemini.com>
2021-12-07 13:37:37 +0530

    Added cat category information

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <CatInformation></CatInformation>
+</template>
+
+<script>
+import CatInformation from "./components/CatInformation";
+export default {
+  name: "App",
+  components: {
+    CatInformation
+  },
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  }
+};
+</script>
+<style>
+
+</style>
